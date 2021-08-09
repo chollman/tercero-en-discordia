e009ddf0aa9103982f9ebbd18a74c74b80283b7b
Claudio Hollman <hollman@strands.com>
2021-08-09 14:44:29 -0300

    Added Redux devtools

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./state/reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
