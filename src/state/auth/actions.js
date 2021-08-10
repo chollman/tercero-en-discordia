5de98bd43339a8ec25c66c943363252d56839d69
Claudio Hollman <hollman@strands.com>
2021-08-10 16:20:42 -0300

    Config API_URL

--- src/state/auth/actions.js
+++ src/state/auth/actions.js
@@ -1,9 +1,11 @@
 import { AUTH_USER, AUTH_ERROR } from "../constants";
 import axios from "axios";
 
+const API_URL = process.env.API_URL || "http://localhost:5000";
+
 export const signup = (formProps, callback) => async (dispatch) => {
     try {
-        const response = await axios.post("http://localhost:5000/signup", formProps);
+        const response = await axios.post(API_URL + "/signup", formProps);
         dispatch({ type: AUTH_USER, payload: response.data.token });
         localStorage.setItem("token", response.data.payload);
         callback();
@@ -23,7 +25,7 @@ export const logout = (callback) => {
 
 export const login = (formProps, callback) => async (dispatch) => {
     try {
-        const response = await axios.post("http://localhost:5000/signin", formProps);
+        const response = await axios.post(API_URL + "/signin", formProps);
         dispatch({ type: AUTH_USER, payload: response.data.token });
         localStorage.setItem("token", response.data.payload);
         callback();
