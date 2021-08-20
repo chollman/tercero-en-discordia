19ada984cd686d3f18f3c3ab5f19063933611017
Claudio Hollman <claudiohollman@gmail.com>
2021-08-20 20:37:27 -0300

    Login form styles

--- src/views/auth/components/login.js
+++ src/views/auth/components/login.js
@@ -3,6 +3,11 @@ import { Form, Field } from "react-final-form";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { login } from "../../../state/auth/actions";
+import FormBootstrap from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -23,27 +28,49 @@ const Login = () => {
         document.title = "Login | Editorial TED";
     }, []);
 
+    const TextFieldAdapter = ({ input, meta, ...rest }) => <FormBootstrap.Control {...input} {...rest} />;
+
     return (
-        <Form
-            onSubmit={onSubmit}
-            validate={validate}
-            render={({ handleSubmit, form, submitting }) => (
-                <form onSubmit={handleSubmit}>
-                    <fieldset>
-                        <label>Email</label>
-                        <Field name="email" component="input" type="text" placeholder="Email" />
-                    </fieldset>
-                    <fieldset>
-                        <label>Contraseña</label>
-                        <Field name="password" component="input" type="password" placeholder="Contraseña" />
-                    </fieldset>
-                    <div>{errorMessage}</div>
-                    <button type="submit" disabled={submitting}>
-                        Login
-                    </button>
-                </form>
-            )}
-        />
+        <section className="main-section main-info">
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md={3}>
+                        <Form
+                            onSubmit={onSubmit}
+                            validate={validate}
+                            render={({ handleSubmit, form, submitting }) => (
+                                <div>
+                                    <FormBootstrap onSubmit={handleSubmit}>
+                                        <FormBootstrap.Group className="mb-3" controlId="formBasicEmail">
+                                            <FormBootstrap.Label>Email</FormBootstrap.Label>
+                                            <Field
+                                                name="email"
+                                                component={TextFieldAdapter}
+                                                type="text"
+                                                placeholder="Email"
+                                            />
+                                        </FormBootstrap.Group>
+
+                                        <FormBootstrap.Group className="mb-3" controlId="formBasicPassword">
+                                            <FormBootstrap.Label>Password</FormBootstrap.Label>
+                                            <Field
+                                                name="password"
+                                                component={TextFieldAdapter}
+                                                type="password"
+                                                placeholder="Contraseña"
+                                            />
+                                        </FormBootstrap.Group>
+                                        <Button variant="primary" type="submit" disabled={submitting}>
+                                            Submit
+                                        </Button>
+                                    </FormBootstrap>
+                                </div>
+                            )}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
 };
 
