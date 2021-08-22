b62588443579e9db8783ae1db733ee270c6b1aa1
Claudio Hollman <claudiohollman@gmail.com>
2021-08-22 18:29:52 -0300

    Fix on token saved in localStorage

--- src/state/auth/actions.js
+++ src/state/auth/actions.js
@@ -7,7 +7,7 @@ export const signup = (formProps, callback) => async (dispatch) => {
     try {
         const response = await axios.post(API_URL + "/signup", formProps);
         dispatch({ type: AUTH_USER, payload: response.data.token });
-        localStorage.setItem("token", response.data.payload);
+        localStorage.setItem("token", response.data.token);
         callback();
     } catch (e) {
         dispatch({ type: AUTH_ERROR, payload: "El mail ya está en uso" });
@@ -25,10 +25,9 @@ export const logout = (callback) => {
 
 export const login = (formProps, callback) => async (dispatch) => {
     try {
-        console.log(process.env);
         const response = await axios.post(API_URL + "/signin", formProps);
         dispatch({ type: AUTH_USER, payload: response.data.token });
-        localStorage.setItem("token", response.data.payload);
+        localStorage.setItem("token", response.data.token);
         callback();
     } catch (e) {
         dispatch({ type: AUTH_ERROR, payload: "Las credenciales no son válidas" });
