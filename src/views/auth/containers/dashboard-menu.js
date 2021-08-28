6268e111efc3bdc28b55d2bd2d4cd428d89207dc
Claudio Hollman <claudiohollman@gmail.com>
2021-08-28 13:07:45 -0300

    Deleted Accordion from admin menu

--- src/views/auth/containers/dashboard-menu.js
+++ src/views/auth/containers/dashboard-menu.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import { MDBAccordion, MDBAccordionItem } from "mdb-react-ui-kit";
 import Button from "react-bootstrap/Button";
 
 const DashboardMenu = ({ onCreateCategoryClicked }) => {
@@ -11,21 +10,6 @@ const DashboardMenu = ({ onCreateCategoryClicked }) => {
             <Button size="sm" variant="outline-primary" onClick={() => onCreateCategoryClicked(2)} block>
                 Autores
             </Button>
-
-            {/*<MDBAccordion initialActive="accordionCollapse1">*/}
-            {/*    <MDBAccordionItem collapseId="accordionCollapse1" headerTitle="Blog - Categorías">*/}
-            {/*        <Button size="sm">Crear</Button>*/}
-            {/*        <Button size="sm">Ver/Editar</Button>*/}
-            {/*    </MDBAccordionItem>*/}
-            {/*    <MDBAccordionItem collapseId="accordionCollapse2" headerTitle="Blog - Tags">*/}
-            {/*        <Button size="sm">Crear</Button>*/}
-            {/*        <Button size="sm">Ver/Editar</Button>*/}
-            {/*    </MDBAccordionItem>*/}
-            {/*    <MDBAccordionItem collapseId="accordionCollapse3" headerTitle="Blos - Posts">*/}
-            {/*        <Button size="sm">Crear</Button>*/}
-            {/*        <Button size="sm">Ver/Editar</Button>*/}
-            {/*    </MDBAccordionItem>*/}
-            {/*</MDBAccordion>*/}
         </div>
     );
 };
