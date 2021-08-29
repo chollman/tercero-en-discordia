5a1e34ae08a7bfb5744e4614be791fcb5fbce2e6
Claudio Hollman <claudiohollman@gmail.com>
2021-08-29 13:26:46 -0300

    Textfield fix to be able to control them via state

--- src/ui/forms/text-field-adapter.js
+++ src/ui/forms/text-field-adapter.js
@@ -1,10 +1,17 @@
 import React from "react";
 import FormBootstrap from "react-bootstrap/Form";
 
-const TextFieldAdapter = ({ input, meta, ...rest }) => {
+const TextFieldAdapter = ({ input, meta, inputOnChange, ...rest }) => {
+    const inputProps = {
+        ...input,
+        onChange: (e) => {
+            input.onChange(e);
+            inputOnChange && inputOnChange(e);
+        },
+    };
     return (
         <span className="my-form-group">
-            <FormBootstrap.Control {...input} {...rest} />
+            <FormBootstrap.Control {...inputProps} value={input.value} {...rest} />
             {meta.error && meta.touched && <div className="error-input">{meta.error}</div>}
         </span>
     );
--- src/ui/forms/text-field-validated-adapter.js
+++ src/ui/forms/text-field-validated-adapter.js
@@ -1,11 +1,26 @@
 import FormBootstrap from "react-bootstrap/Form";
 import React from "react";
 
-const TextFieldValidatedAdapter = ({ input, meta, ...rest }) => (
-    <span className="my-form-group">
-        <FormBootstrap.Control isInvalid={meta.error && meta.touched} isValid={!meta.error} {...input} {...rest} />
-        {meta.error && meta.touched && <div className="error-input">{meta.error}</div>}
-    </span>
-);
+const TextFieldValidatedAdapter = ({ input, meta, inputOnChange, ...rest }) => {
+    const inputProps = {
+        ...input,
+        onChange: (e) => {
+            input.onChange(e);
+            inputOnChange && inputOnChange(e);
+        },
+    };
+    return (
+        <span className="my-form-group">
+            <FormBootstrap.Control
+                {...inputProps}
+                value={input.value}
+                isInvalid={meta.error && meta.touched}
+                isValid={!meta.error}
+                {...rest}
+            />
+            {meta.error && meta.touched && <div className="error-input">{meta.error}</div>}
+        </span>
+    );
+};
 
 export default TextFieldValidatedAdapter;
