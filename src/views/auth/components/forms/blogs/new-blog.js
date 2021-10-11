2b503a15e5c841af33598f358d83c6a240a7f1d4
Gonzalo Bonora <bonora@strands.com>
2021-10-11 01:39:24 -0300

    Left some ideas and TODOs in comments

--- src/views/auth/components/forms/blogs/new-blog.js
+++ src/views/auth/components/forms/blogs/new-blog.js
@@ -4,6 +4,30 @@ import { EditorState, convertToRaw } from "draft-js";
 import "../../../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css"; //This is import gives me pain
 import { Button } from "react-bootstrap";
 
+/*
+    Necesitamos un componente editor para:
+    1) Crear un blogpost
+    2) Editar un blogpost que ya existe
+
+    Para el caso 1) el State del Editor se inicia vacío
+    Para el caso 2) tenemos que traerlo desde el endpoint
+
+    Vamos a guardar en la DB el contenido de editorState.getCurrentContent() convertido a raw con la función
+    convertToRaw. Y cuando lo traemos de DB, lo convertimos de nuevo con la función convertFromRaw.
+
+    TODO:
+    - Abstraer toda la lógica del editor a un componente editor, para reutilizarlo en los casos 1) y 2)
+    - Crear estilos css para mejorar las visuales
+    - Hacer Actions, Reducers, API calls, para guardar el blogpost vía backend, y para traer
+    un blogpost desde backend
+    - Agregar campos que faltan en la UI:
+            -title
+            -categories
+            -author
+            -comments
+            -tags
+ */
+
 const NewBlog = () => {
     const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
 
